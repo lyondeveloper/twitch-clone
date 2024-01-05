19bf00f7f5147191f0e7ea94e0530f00e5ccee30
jrincon <jesus.rinconice.com>
2024-01-04 23:59:49 -0400

    feat: chat settings page

--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -2,7 +2,6 @@ import { isFollowingUser } from "@/lib/follow-service";
 import { getUserByUsername } from "@/lib/user-service";
 import { notFound } from "next/navigation";
 import { Actions } from "./_components/actions";
-import { isBlockedByUser } from "@/lib/block-service";
 
 interface UserPageProps {
   params: {
@@ -18,7 +17,6 @@ const UserPage = async ({ params: { username } }: UserPageProps) => {
   }
 
   const isFollowing = await isFollowingUser(user.id);
-  const isBlocked = await isBlockedByUser(user.id);
 
   return (
     <div className="flex flex-col gap-y-4">
