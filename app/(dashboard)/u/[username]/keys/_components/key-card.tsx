51d23cc47c1a4476c53fe5d9d4a33993f884cecc
jrincon <jesus.rinconice.com>
2024-01-09 11:29:36 -0400

    feat: key settings

---
+++ app/(dashboard)/u/[username]/keys/_components/key-card.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { Input } from "@/components/ui/input";
+import { CopyButton } from "./copy-button";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+interface KeyCardProps {
+  value: string | null;
+}
+
+export const KeyCard = ({ value }: KeyCardProps) => {
+  const [show, setShow] = useState(false);
+  return (
+    <div className="rounded-xl bg-muted p-6">
+      <div className="flex items-center gap-x-10">
+        <p className="font-semibold shrink-0">
+          Server URL
+        </p>
+        <div className="space-y-2 w-full">
+          <div className="w-full flex items-center gap-x-2">
+            <Input value={value || ''} disabled placeholder="Server URL" type={show ? "text" : "password"} />
+            <CopyButton value={value || ''} />
+          </div>
+          <Button onClick={() => setShow(!show)} size="sm" variant="link"> {show ? "Hide" : "Show"} </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
