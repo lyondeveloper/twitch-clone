cf78edc323e5e9c56937e615121acb822c4a529b
jrincon <jesus.rinconice.com>
2024-01-18 23:45:30 -0400

    feat: video component

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -3,6 +3,7 @@
 import { useViewerToken } from "@/hooks/useViewerToken";
 import { Stream, User } from "@prisma/client";
 import { LiveKitRoom } from '@livekit/components-react';
+import { Video } from "./video";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null; };
@@ -26,6 +27,19 @@ export const StreamPlayer = ({ user, stream, isFollowing }: StreamPlayerProps) =
   } 
 
   return (
-    <div>allowed to watch the stream</div>
+    <>
+      <LiveKitRoom
+        token={token}
+        serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
+        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 2xl:grid-cols-6 h-full"
+      >
+        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+          <Video 
+            hostName={user.username}
+            hostIdentity={user.id}
+          />
+        </div>
+      </LiveKitRoom>
+    </>
   )
 }
