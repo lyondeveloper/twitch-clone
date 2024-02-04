4c8559aba6f5d3f38e814623586ef585f4c7ed94
jrincon <jesus.rinconice.com>
2024-02-03 21:00:30 -0400

    feat: homepage added

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Feed, FeedSkeleton } from './_components/feed';
 
 const HomePage = () => {
   return (
-    <div>
-      HomePage
+    <div className='h-full p-8 max-w-screen-2xl mx-auto'>
+      <Suspense fallback={<FeedSkeleton />}>
+        <Feed />
+      </Suspense>
     </div>
   )
 }
